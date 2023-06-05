3bee976829c0e05ec67343d8f6e34d004b8cf204
ipcampbell12 <ipcampbell12@gmail.com>
2023-06-05 09:22:50 -0700

    helper functions and post endpoint

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 
-import express from "express";
-
-const app = express();
+const express = require('express');
 const Joi = require('joi');
 
+const app = express()
 app.use(express.json());
 
 
@@ -14,12 +13,21 @@ const genres = [
     { id: 4, name: 'Comedy' },
 ];
 
+//helper functions
 const findGenre = (id) => genres.find(genre => genre.id === parseInt(id))
+
+const validateGenre = (course) => {
+    const schema = Joi.object({
+        name: Joi.string().min(6).required()
+    })
+
+    return schema
+}
 
 
 app.get('/api/genres', (req, res) => {
     res.send(genres)
-})
+});
 
 
 app.get('/api/genres/:id', (req, res) => {
@@ -27,4 +35,31 @@ app.get('/api/genres/:id', (req, res) => {
     if (!genre) return res.status(404).send(`There is no genre with ID ${req.params.id}`)
 
     res.send(genre)
+});
+
+
+app.post('/api/genres', (req, res) => {
+
+    const { error } = validateGenre(req.body)
+
+    if (error) return res.status(400).send(error.details[0].message)
+
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    }
+
+    genres.push(genre)
+    res.send(genre)
+})
+
+app.put()
+
+app.delete()
+
+//in terminal: export PORT=5001
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
 })
