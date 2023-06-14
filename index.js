3821571fff493808f80ad3f32b9911a112940cc7
ipcampbell12 <ipcampbell12@gmail.com>
2023-06-14 09:31:24 -0700

    separe geners module

--- index.js
+++ index.js
@@ -14,69 +14,6 @@ const genres = [
     { id: 4, name: 'Comedy' }
 ];
 
-//helper functions
-const findGenre = (id) => genres.find(genre => genre.id === parseInt(id))
-
-const validateGenre = (genre) => {
-    const schema = Joi.object({
-        name: Joi.string().min(6).required()
-    })
-
-    return schema.validate(genre)
-}
-
-
-app.get('/api/genres', (req, res) => {
-    res.send(genres)
-});
-
-
-app.get('/api/genres/:id', (req, res) => {
-    const genre = findGenre(req.params.id)
-    if (!genre) return res.status(404).send(`There is no genre with ID ${req.params.id}`)
-
-    res.send(genre)
-});
-
-
-app.post('/api/genres', (req, res) => {
-
-    const { error } = validateGenre(req.body)
-
-    if (error) return res.status(400).send(error.details[0].message)
-
-    const genre = {
-        id: genres.length + 1,
-        name: req.body.name
-    }
-
-    genres.push(genre)
-    res.send(genre)
-});
-
-app.put('/api/genres/:id', (req, res) => {
-
-    const genre = findGenre(req.params.id)
-    if (!genre) return res.status(404).send(`There is no genre with ID ${req.params.id}`)
-
-    const { error } = validateGenre(req.body)
-
-    if (error) return res.status(400).send(error.details[0].message)
-
-    genre.name = req.body.name;
-
-    res.send(genre)
-});
-
-app.delete('/api/genres/:id', (req, res) => {
-    const genre = findGenre(req.params.id)
-    if (!genre) return res.status(404).send(`There is no genre with ID ${req.params.id}`)
-
-    const genreIndex = genres.indexOf(genre)
-    genres.splice(genreIndex, 1)
-
-    res.send(genre)
-})
 
 //in terminal: export PORT=5001
 const port = process.env.PORT || 3000;
