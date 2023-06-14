4a208e7fdaa77618c10844ccf297b12430ca06fb
ipcampbell12 <ipcampbell12@gmail.com>
2023-06-14 09:36:06 -0700

    changed port

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use('/api/genres', genres)
 
 
 //in terminal: export PORT=5001
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
